09426bb7f669eaeb716f0d2896c8c23d174fa36f
adrientremblay <adrien@adrientremblay.com>
2020-03-16 18:28:03 -0400

    Symbolic constants

--- convert_book.c
+++ convert_book.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 /* print Fahrenheit-Celsius table
  * for fahr = 0, 20, ..., 300 */
 int main() {
   int fahr, celsius;
-  int lower, upper, step;
 
-  lower = 0; /* lower limit of temperature table */
-  upper = 300; /* upper limit */
-  step = 20; /* step size */
-
-  fahr = lower;
-  while (fahr <= upper) {
+  fahr = LOWER;
+  while (fahr <= UPPER) {
     celsius = 5 * (fahr-32) / 9;
     printf("%d\t%d\n", fahr, celsius);
-    fahr = fahr + step;
+    fahr = fahr + STEP;
   }
    return 0;
 }
