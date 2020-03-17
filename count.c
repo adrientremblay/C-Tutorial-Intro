cff509950ad36e8bb2060b07d2239e499edffb28
adrientremblay <adrien@adrientremblay.com>
2020-03-16 20:14:23 -0400

    Program to count lines

--- count.c
+++ count.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-/* count characters in input; 1st version */
+/* count characters in input; 2nd version */
 int main()
 {
-    long nc;
-    nc=0;
+    double nc;
 
-    while (getchar() != EOF) nc++;
-    printf("%ld\n", nc);
+    for (nc = 0 ; getchar() != EOF ; nc++);
+    printf("%.0f\n", nc);
     
     return 0;
 }
